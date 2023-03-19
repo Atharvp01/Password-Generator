860a3ab4b63937bb83dd384f3eb3771b4e01ff71
Atharv <atharvpatil5177@gmail.com>
2023-03-20 00:41:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Let's first store all the lowercase character in a variable
+
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+//Now let's create a variable containing uppercase characters
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//Let's store 0-9 numbers in a variable
+
+const numbers = "0123456789";
